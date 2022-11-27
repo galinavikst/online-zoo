c2dc31afe957a3122e0e36463a01ed18aad250a9
galinavikst <galinavikst@gmail.com>
2022-11-27 20:11:25 +0100

    fix: fix footer margins media 640px

--- pages/petstory/script.js
+++ pages/petstory/script.js
@@ -1,29 +1,41 @@
 let header = document.querySelector(".header");
 let headerBox = document.querySelector(".header-box");
 let headerNav = document.querySelector("#header-nav");
+let headerLogo = document.querySelector(".header-logo");
 let navList = document.querySelector(".nav-list");
 let hambMenu = document.querySelector("#hamb-menu");
-function showHambMenu() {
-  if (hambMenu.checked) {
-    header.style.padding = "0 30px 40px";
-    headerBox.style.display = "block";
-    headerNav.style.display = "block";
-    navList.style.display = "block";
-  } else {
-    hideHambMenu();
-  }
-}
-hambMenu.addEventListener("click", showHambMenu);
-function hideHambMenu() {
-  header.style.padding = "0 30px";
-  headerBox.style.display = "flex";
-  headerNav.style.display = "none";
-  navList.style.display = "none";
-}
-window.addEventListener("resize", function () {
-  if (window.matchMedia("(max-width: 640px)").matches) {
-    hideHambMenu();
-  } else {
-    hambMenu.addEventListener("click", showHambMenu);
-  }
-});
+//
+// hambMenu.addEventListener("click", function () {
+//   if (hambMenu.checked) {
+//     showHambMenu();
+//   } else {
+//     hideHambMenu();
+//   }
+// });
+// function showHambMenu() {
+//   header.style.padding = "0 30px 40px";
+//   headerLogo.style.textAlign = "left";
+//   headerBox.style.display = "block";
+//   headerNav.style.display = "block";
+//   navList.style.display = "block";
+// }
+// function hideHambMenu() {
+//   header.style.padding = "0 30px";
+//   headerBox.style.display = "flex";
+//   headerNav.style.display = "none";
+//   navList.style.display = "none";
+// }
+//
+// window.addEventListener("resize", function () {
+//   if (window.matchMedia("(max-width: 640px)").matches) {
+//     hideHambMenu();
+//     hambMenu.checked = false;
+//   }
+// });
+// window.addEventListener("resize", function () {
+//   if (window.matchMedia("(min-width: 641px)").matches) {
+//     headerBox.style.display = "flex";
+//     headerNav.style.display = "flex";
+//     navList.style.display = "flex";
+//   }
+// });
